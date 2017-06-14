aa3057f0231699e4edec27e4da06c4583823f96d
nitg16 <niteshgupta16@gmail.com>
2017-06-14 12:56:49 +0530

    Fixes #2

--- TabPad.py
+++ TabPad.py
@@ -9,8 +9,10 @@
 import sys
 from PyQt5 import QtCore
 from PyQt5.QtWidgets import QWidget, QApplication, QPushButton
+from PyQt5.QtWidgets import QSystemTrayIcon, QStyle, QAction, QMenu
 import subprocess
 from TabPadConfig import *
+from PyQt5.QtGui import QCursor, QIcon
 
 # todo
 # implement individual button sizes in dict
@@ -31,12 +33,14 @@ class TabPad(QWidget):
 		+ str(button_border_radius) + "px;border-color:" + str(button_border_color) \
 		+ ";}"
 		self.setStyleSheet(self.stl)
+		self.appicon = QIcon.fromTheme("input-gaming")
 		self.initUI()
 
 	def initUI(self):
 		for k, v in button_layout.items():
 			self.createandmove(k, v[0], v[1], v[2], v[3])
 		self.createandmove("Quit", 35, 80, [], "#808080")
+		self.systraysetup()
 		self.setGeometry(overlay_x_position, overlay_y_position, overlay_width, overlay_height)
 		self.setWindowTitle('TabPad')
 		self.show()
@@ -62,8 +66,7 @@ class TabPad(QWidget):
 	def keyhandler(self, lbl):
 		def processinput():
 			if lbl == "Quit":
-				QtCore.QCoreApplication.instance().quit()
-				sys.exit(1)
+				self.quithandler()
 			else:
 				cmnd = button_layout[lbl][2]
 				subprocess.Popen(cmnd, stdout=subprocess.PIPE)
@@ -73,6 +76,31 @@ class TabPad(QWidget):
 		h = hexcolor.strip('#')
 		h = (tuple(int(h[i:i+2], 16) for i in (0, 2 ,4)))
 		return (str(h[0]) + "," + str(h[1]) + "," + str(h[2]))
+
+	def systraysetup(self):
+		self.tray_icon = QSystemTrayIcon(self)
+		self.tray_icon.setIcon(self.appicon)
+		self.show_action = QAction("Show", self)
+		self.quit_action = QAction("Exit", self)
+		self.hide_action = QAction("Hide", self)
+		self.show_action.triggered.connect(self.show)
+		self.hide_action.triggered.connect(self.hide)
+		self.quit_action.triggered.connect(self.quithandler)
+		self.tray_menu = QMenu()
+		self.tray_menu.addAction(self.show_action)
+		self.tray_menu.addAction(self.hide_action)
+		self.tray_menu.addAction(self.quit_action)
+		self.tray_icon.setContextMenu(self.tray_menu)
+		self.tray_icon.show()
+		self.tray_icon.activated.connect(self.catchclick)
+
+	def catchclick(self, value):
+		if value == self.tray_icon.Trigger: #left click!
+			self.tray_menu.exec_(QCursor.pos())
+
+	def quithandler(self):
+		QtCore.QCoreApplication.instance().quit()
+		sys.exit(1)
 
 def main():
 	app = QApplication(sys.argv)
