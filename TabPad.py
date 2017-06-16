9109d77b468f3973426832405e84484a3665ba73
nitg16 <niteshgupta16@gmail.com>
2017-06-16 14:48:05 +0530

    Qt events are useless in our case, using evdev instead

--- TabPad.py
+++ TabPad.py
@@ -13,33 +13,44 @@ from PyQt5.QtWidgets import QSystemTrayIcon, QStyle, QAction, QMenu
 import subprocess
 from TabPadConfig import *
 from PyQt5.QtGui import QCursor, QIcon, QKeyEvent
+import time
+import evdev
+import threading
+from evdev import ecodes
+from decimal import Decimal
 
 class TabPad(QWidget):
 	def __init__(self):
 		super(TabPad, self).__init__()
 		self.setWindowFlags(QtCore.Qt.WindowStaysOnTopHint |
 							QtCore.Qt.FramelessWindowHint |
-							QtCore.Qt.X11BypassWindowManagerHint
+							QtCore.Qt.X11BypassWindowManagerHint |
+							QtCore.Qt.WA_ShowWithoutActivating |
+							QtCore.Qt.WindowDoesNotAcceptFocus
 							)
 		if transparent_background:
 			self.setAttribute(QtCore.Qt.WA_TranslucentBackground)
+		self.setFocusPolicy(QtCore.Qt.NoFocus)	
 		self.appicon = QIcon.fromTheme("input-gaming")
-		self.pressed_keylist = []
-		self.first_released = False
+		self.screen_resolution = QApplication.desktop().screenGeometry()
+		self.screen_width, self.screen_height = self.screen_resolution.width(), self.screen_resolution.height()
+		self.eventThread = newThread(1, "Event Thread 1", touch_panel, self.screen_width, self.screen_height)
+		self.eventThread.start()
 		self.initUI()
 
 	def initUI(self):
+		if hide_on_close:
+			button_layout["Hide"] = button_layout.pop("Close_")
+		else:
+			button_layout["Close"] = button_layout.pop("Close_")
+
 		for k, v in button_layout.items():
 			self.createandmove(k, *v)
-		if hide_on_close:
-			self.createandmove("Hide", 45, 5, [], "#808080", (70,30))
-		else:
-			self.createandmove("Close", 45, 5, [], "#808080", (70, 30))
 		self.systraysetup()
 		self.setGeometry(overlay_x_position, overlay_y_position, overlay_width, overlay_height)
 		self.setWindowTitle('TabPad')
 		self.show()
-		self.activateWindow()
+		# self.activateWindow()
 
 	def createandmove(self, label, xper, yper, command, color, btnsize):
 		self.qbtn = QPushButton(label, self)
@@ -70,9 +81,6 @@ class TabPad(QWidget):
 				self.hide()
 			elif not hide_on_close and lbl == "Close":
 				self.quithandler()
-			else:
-				cmnd = button_layout[lbl][2]
-				subprocess.Popen(cmnd, stdout=subprocess.PIPE)
 		return processinput
 
 	def hextorgb(self, hexcolor):
@@ -105,23 +113,110 @@ class TabPad(QWidget):
 		QtCore.QCoreApplication.instance().quit()
 		sys.exit(1)
 
-	def keyPressEvent(self, event):
-		self.first_released = True
-		self.key_input = str(event.key())
-		self.pressed_keylist.append(self.key_input)
-
-	def keyReleaseEvent(self, event):
-		if self.first_released == True: 
-			self.process_multiple_keys(self.pressed_keylist)
-		self.first_released = False
-		del self.pressed_keylist[-1]
-
-	def process_multiple_keys(self,keys_pressed):
-		print (keys_pressed)
-
 	def showpad(self):
 		self.show()
-		self.activateWindow()
+		# self.activateWindow()
+
+class newThread (threading.Thread):
+	def __init__(self, threadID, name, touch_panel, screen_width, screen_height):
+		threading.Thread.__init__(self)
+		self.threadID = threadID
+		self.name = name
+		self.daemon = True
+		self.touch_panel = touch_panel
+		self.no_device_found = True
+		self.screen_width = screen_width
+		self.screen_height= screen_height
+		self.devices = [evdev.InputDevice(fn) for fn in evdev.list_devices()]
+		# print (self.devices)
+		self.inputsetup()
+
+	def run(self):
+		print ("Starting " + self.name)
+		self.eventloop()
+
+	def inputsetup(self):
+		for d in self.devices:
+			# print(d.fn, d.name, device.phys)
+			# print (d.name)
+			if (d.name == self.touch_panel):
+				input_node = d.fn
+				self.no_device_found = False
+
+		if self.no_device_found == True:
+			print ("No touch input detected.")
+			print ("Please make sure you have entered correct touch panel name in user settings.")
+			print ("And that you are running the app with root access.")
+			print ("Quitting in 10 seconds.")
+			time.sleep(10)
+			quit()
+			sys.exit(1)
+
+		self.device = evdev.InputDevice(input_node)
+
+	def eventloop(self):
+		touch_time = None
+		lift_time = None
+		x_abs_val = None
+		y_abs_val = None
+		for ev in self.device.read_loop():
+			# print (evdev.util.categorize(ev))
+			if ev.code == 330 and ev.value == 1:
+				touch_time = Decimal(str(ev.sec) + "." + str(ev.usec))
+				# print touch_time
+
+			if ev.code == 330 and ev.value == 0:
+				lift_time = Decimal(str(ev.sec) + "." + str(ev.usec))
+				# print lift_time
+			else:
+				lift_time = None
+
+			if ev.code == 0:
+				if ev.value > 0:
+					x_abs_val = ev.value
+			if ev.code == 1:
+				if ev.value > 0:
+					y_abs_val = ev.value
+
+			if lift_time != None and x_abs_val != None and y_abs_val != None:
+				self.compare_coords(self.xdotool_coords()[0], self.xdotool_coords()[1])
+
+	def percentconvertor(self, val, dimension):
+		val = int(round((dimension * val)/100))
+		return val
+
+	def compare_coords(self, actual_x, actual_y):
+		for k, v in button_layout.items():
+			self.x_start_pos = self.percentconvertor(v[0], overlay_width)
+			self.x_end_pos = self.x_start_pos + v[4][0]
+			self.y_start_pos = self.percentconvertor(v[1], overlay_height)
+			self.y_end_pos = self.y_start_pos + v[4][1]
+			if actual_x >= self.x_start_pos and actual_x <= self.x_end_pos:
+				if actual_y >= self.y_start_pos and actual_y <= self.y_end_pos:
+					# print (v[2])
+					self.command_executor(v[2])
+
+	def xdotool_coords(self):
+		proc = subprocess.Popen("eval $(xdotool getmouselocation --shell) && echo $X $Y", stdout=subprocess.PIPE, stderr=subprocess.PIPE, shell = True ).communicate()
+		p = str(proc[0]).strip("\\n'")
+		p = p.strip("b'") 
+		xc = int(p.partition(' ')[0])
+		yc = int(p.partition(' ')[2])
+		# print (xc, yc)
+		if overlay_width < self.screen_width:
+			xc = xc - overlay_x_position
+			if xc < 0:
+				xc = 0
+		if overlay_height < self.screen_height:
+			yc = yc - overlay_y_position
+			if yc < 0:
+				yc = 0
+			return (xc, yc)
+
+	def command_executor(self, command_array):
+		if command_array:
+			subprocess.Popen(command_array, stdout=subprocess.PIPE)
+
 
 def main():
 	app = QApplication(sys.argv)
--- TabPadConfig.py
+++ TabPadConfig.py
@@ -2,6 +2,10 @@
 
 # Change Settings in this file
 
+# Enter your touch input name below (can be found out by running xinput command)
+# Make sure enter this value correctly or the app will fail immediately after launch.
+touch_panel = "GDIX1000:00 27C6:1002"
+
 # Enable or disable transparent background.
 # It can be useful in precise positioning of buttons.
 transparent_background = True
@@ -10,7 +14,7 @@ transparent_background = True
 overlay_x_position = 0
 overlay_y_position = 300
 
-# Width and height of transparent overlay window
+# Width and height of transparent overlay window (DO NOT set these more than your max screen resolution)
 overlay_width = 1024
 overlay_height = 400
 
@@ -49,6 +53,8 @@ hide_on_close = True
 # Make sure you do it in the format shown below to prevent app crashes.
 # Don't forget to put a comma after each entry, except for the last one.
 
+# DO NOT change any label ending with underscore, e.g. "Close_"
+
 button_layout = {
 	"L1": (6, 5, ["xdotool", "key", "q"], "#ff0000", (70, 40)),
 	"L2": (14, 5, ["xdotool", "key", "w"], "#ff0000", (70, 40)),
@@ -63,5 +69,6 @@ button_layout = {
 	"1": (83, 80, ["xdotool", "key", "k"], "#0000ff", (60, 60)),
 	"4": (75, 70, ["xdotool", "key", "j"], "#ffc0cb", (60, 60)),
 	"Start": (40, 92, ["xdotool", "key", "v"], "#ffa500", (70, 30)),
-	"Select": (50, 92, ["xdotool", "key", "n"], "#800080", (70, 30))
+	"Select": (50, 92, ["xdotool", "key", "n"], "#800080", (70, 30)),
+	"Close_": (45, 5, [], "#808080", (70, 30))
 }
